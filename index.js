759253b6e19e38792fbe28a7ecf56e9ef607e3f5
cristianCor19 <cordobac96@gmail.com>
2025-02-13 00:33:54 -0500

    feat: configurar servidor y opciones de CORS, y reorganizar estructura de archivos

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import userRoutes from "./V1/routes/user.routes.js";
-import authRoutes from "./V1/routes/auth.routes.js";
-import { connectDatabase } from './database/connect_db.js';
+import app from "./config/server.js";
+import { PORT } from "./config/env.js";
 
-const app = express();
-
-const corsOptions = {
-  origin: '',
-  credentials: true
-}
-
-app.use(cors(corsOptions));
-//Call the environment variables
-dotenv.config();
-
-app.set('port', process.env.PORT)
-app.use(express.json());
-
-connectDatabase();
-
-app.use('/user', userRoutes)
-app.use('/auth', authRoutes)
-
-
-//Start the server
-
-app.listen(app.get('port'), () => {
-  console.log(`server listening to port : ${app.get('port')}`);
-})
-
+app.listen(PORT, () => {
+  console.log(`Servidor funcioando sobre el puerto : ${PORT}`);
+});
